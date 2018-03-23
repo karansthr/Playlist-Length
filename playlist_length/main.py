27d3b218131e95ca3804566c1166cfff87d75413
Mohit <mohitsolanki619@gmail.com>
2018-03-23 21:42:00 +0530

    Add Progress Bar

--- playlist_length/main.py
+++ playlist_length/main.py
@@ -6,6 +6,7 @@ from concurrent.futures import ProcessPoolExecutor
 
 import huepy
 import magic
+from tqdm import tqdm
 
 
 def probe(vid_file_path):
@@ -69,21 +70,32 @@ def main():
     BASE_PATH = args.path
 
     if not os.path.isdir(BASE_PATH):
-        print('Give the path to directory as an argument '\
-              'or nothing for current directory.')
+        print(
+            'Give the path to directory as an argument '
+            'or nothing for current directory.'
+        )
         exit()
-    all_files = (
+    all_files = [
         os.path.join(root, file)
         for root, _, files in os.walk(BASE_PATH)
         for file in files
-    )
+    ]
+    print()
     with ProcessPoolExecutor() as executor:
-        result = executor.map(duration, all_files)
+        result = list(
+            tqdm(
+                executor.map(duration, all_files),
+                total=len(all_files),
+                ascii=True,
+                desc='Please Wait',
+            )
+        )
     length = round(sum(result))
     if not length:
         return huepy.bold(
-            huepy.red('Seems like there is no video  ¯\_(ツ)_/¯')
+            huepy.red('\nSeems like there is no video  ¯\_(ツ)_/¯\n')
         )
+
     if length < 60:
         message = 'Length of all vidoes is {} minutes.'.format(length)
     else:
@@ -92,7 +104,7 @@ def main():
             hours, minutes
         )
     message = huepy.bold(huepy.green(message))
-    return message
+    return f'\n{message}\n'
 
 
 if __name__ == '__main__':
