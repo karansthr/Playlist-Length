7d5d9eadeb0256d916fbfe57ecaf08a1fabf32b5
Mohit <mohitsolanki619@gmail.com>
2018-03-31 15:38:14 +0530

    Update __init__.py with metadata

--- playlist_length/__init__.py
+++ playlist_length/__init__.py
@@ -1,1 +1,4 @@
-from .main import main
+__version__ = '1.3'
+__author__ = 'Karan Suthar, Mohit Solanki'
+__license__ = 'MIT License'
+__description__ = 'A command-line tool to calculate the length of vidoes in a directory'
