138ad2fe0f27cee84a29dc00b46dc9a1f7377cb2
Mohit <mohitsolanki619@gmail.com>
2018-03-31 15:47:20 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from distutils.core import setup
 
 setup(
     name='playlist-length',
-    version='1.2',
-    description='A simple tool to get total lenght of videos in a directory',
+    version='1.3',
+    description='A command line tool to get length of all videos in a directory',
     long_description=open('README.md').read().strip(),
     author='Karan Suthar',
     author_email='karansthr97@gmail.com',
     url='http://github.com/karansthr/playlist-length',
     packages=['playlist_length'],
-    install_requires=["python-magic","tqdm","huepy"],
+    install_requires=["python-magic", "tqdm", "huepy"],
     license='MIT License',
-    keywords='videolenght playlist-length',
+    keywords='videolength playlist-length',
     entry_points={
-        'console_scripts': ['playlistlen=playlist_length:main'],
+        'console_scripts': ['playlistlen=playlist_length.main:main'],
     },
 )
