ef58df5e770f243e51fbf06ce49a16e785a6425f
karansthr <karansthr97@gmail.com>
2018-04-11 10:37:23 +0530

    remove version redundancy

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
+from playlist_length.__version__ import __version__
 
 requires = ('huepy==0.9.6', 'python-magic>=0.4.15', 'tqdm>=4.19.9')
 
 setup(
     name='playlist-length',
-    version='1.3',
+    version=__version__,
     description='A command-line tool to get length of all videos in a directory',
     long_description=open('README.md').read().strip(),
     author='Karan Suthar',
