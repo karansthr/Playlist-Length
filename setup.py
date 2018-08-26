d8ea7f52df293a2c26e8f26bab4cce57b2c4e834
Karan Suthar <karansthr97@gmail.com>
2018-08-26 11:08:04 +0530

    add Python2 support

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from distutils.core import setup
 from playlist_length.__version__ import __version__
 
 
-requires = ('huepy==0.9.6', 'python-magic>=0.4.15', 'tqdm>=4.19.9')
+requires = ('huepy==0.9.6', 
+            'python-magic>=0.4.15',
+            'tqdm>=4.19.9',
+            'futures;python_version<"3.0"'
+            )
 
 setup(
     name='playlist-length',
