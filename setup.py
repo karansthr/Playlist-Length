3791ea24800f10bf01ca4e9c6525063532b81843
Karan Suthar <karansthr97@gmail.com>
2018-08-26 11:42:12 +0530

    corrected python version in dependency

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from distutils.core import setup
 from playlist_length.__version__ import __version__
 
 
-requires = ('huepy==0.9.6', 
-            'python-magic>=0.4.15',
-            'tqdm>=4.19.9',
-            'futures;python_version<"3.0"'
-            )
+requires = (
+    'huepy==0.9.6',
+    'python-magic>=0.4.15',
+    'tqdm>=4.19.9',
+    'futures;python_version<"3.4"',
+)
 
 setup(
     name='playlist-length',
