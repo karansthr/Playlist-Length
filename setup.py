43cef2b9b417c96b10aefcf693cda4ae7c85adfc
Karan Suthar <karansthr97@gmail.com>
2018-08-29 23:36:51 +0530

    fixed markdown rendering on PyPI

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 from playlist_length.__version__ import __version__
 
@@ -15,6 +15,7 @@ setup(
     version=__version__,
     description='A command-line tool to get length of all videos in a directory',
     long_description=open('README.md').read().strip(),
+    long_description_content_type='text/markdown',
     author='Karan Suthar',
     author_email='karansthr97@gmail.com',
     url='http://github.com/karansthr/playlist-length',
@@ -25,4 +26,10 @@ setup(
     entry_points={
         'console_scripts': ['playlistlen=playlist_length.main:main'],
     },
+    project_urls={
+        'Bug Reports': 'https://github.com/karansthr/playlist-length/issues',
+        'Source': 'https://github.com/karansthr/playlist-length/',
+        'Blog': 'https://fosstack.com/tips/videos-playlist-length-calculator/',
+        'About': 'https://fosstack.com/about/'
+    }
 )
